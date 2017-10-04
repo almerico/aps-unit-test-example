b014ca5303701dc552a147a5535fe1a51108c4aa
cijujoseph <cijujoseph@Cijus-MBP.T-mobile.com>
2017-10-04 14:49:45 -0500

    Added a DMN unit test example

---
+++ src/test/java/com/alfresco/aps/test/process/DmnUnitTest.java
@@ -0,0 +1,30 @@
+package com.alfresco.aps.test.process;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.activiti.dmn.engine.RuleEngineExecutionResult;
+import com.alfresco.aps.testutils.AbstractDmnTest;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:activiti.dmn.cfg.xml" })
+public class DmnUnitTest extends AbstractDmnTest {
+	
+	String decisonTableKey = "dmntest";
+
+	@Test
+	public void testProcessExecution() throws Exception {
+		Map<String, Object> processVariablesInput = new HashMap<>();
+		processVariablesInput.put("input", "xyz");
+		RuleEngineExecutionResult result = ruleService.executeDecisionByKey(decisonTableKey, processVariablesInput);
+		Assert.assertNotNull(result);
+		Assert.assertSame(result.getResultVariables().get("output").getClass(), String.class);
+		Assert.assertEquals(result.getResultVariables().get("output"), "abc");
+	}
+
+}
