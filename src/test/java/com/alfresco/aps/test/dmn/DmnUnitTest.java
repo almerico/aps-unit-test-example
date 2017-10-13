cf60b609f90dd38920651823c829726c96a99b87
cijujoseph <cijujoseph@Cijus-MBP.T-mobile.com>
2017-10-13 09:37:58 -0500

    added documentation

--- src/test/java/com/alfresco/aps/test/dmn/DmnUnitTest.java
+++ src/test/java/com/alfresco/aps/test/dmn/DmnUnitTest.java
@@ -15,14 +15,20 @@ import com.alfresco.aps.testutils.AbstractDmnTest;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:activiti.dmn.cfg.xml" })
 public class DmnUnitTest extends AbstractDmnTest {
-	
+
 	static {
 		appName = "Test App";
 		decisonTableKey = "dmntest";
 	}
 
+	/*
+	 * Test a successful hit using all possible inputs
+	 */
 	@Test
 	public void testDMNExecution() throws Exception {
+		/*
+		 * Invoke with input set to xyz and assert output is equal to abc
+		 */
 		Map<String, Object> processVariablesInput = new HashMap<>();
 		processVariablesInput.put("input", "xyz");
 		RuleEngineExecutionResult result = ruleService.executeDecisionByKey(decisonTableKey, processVariablesInput);
@@ -30,14 +36,20 @@ public class DmnUnitTest extends AbstractDmnTest {
 		Assert.assertEquals(1, result.getResultVariables().size());
 		Assert.assertSame(result.getResultVariables().get("output").getClass(), String.class);
 		Assert.assertEquals(result.getResultVariables().get("output"), "abc");
-		
+
+		/*
+		 * Invoke with input set to 123 and assert output is equal to abc
+		 */
 		processVariablesInput.put("input", "123");
 		result = ruleService.executeDecisionByKey(decisonTableKey, processVariablesInput);
 		Assert.assertNotNull(result);
 		Assert.assertEquals(1, result.getResultVariables().size());
 		Assert.assertSame(result.getResultVariables().get("output").getClass(), String.class);
 		Assert.assertEquals(result.getResultVariables().get("output"), "abc");
-		
+
+		/*
+		 * Invoke with input set to abc and assert output is equal to abc
+		 */
 		processVariablesInput.put("input", "abc");
 		result = ruleService.executeDecisionByKey(decisonTableKey, processVariablesInput);
 		Assert.assertNotNull(result);
@@ -45,7 +57,10 @@ public class DmnUnitTest extends AbstractDmnTest {
 		Assert.assertSame(result.getResultVariables().get("output").getClass(), String.class);
 		Assert.assertEquals(result.getResultVariables().get("output"), "abc");
 	}
-	
+
+	/*
+	 * Test a miss
+	 */
 	@Test
 	public void testDMNExecutionNoMatch() throws Exception {
 		Map<String, Object> processVariablesInput = new HashMap<>();
